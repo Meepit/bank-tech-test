179d779f727295c903cdf7d1c264fddac5fb1525
James Lizamore <jwlizamore@gmail.com>
2018-01-29 16:20:31 +0000

    Added display method to TransactionPrinter

---
+++ jasmine/spec/TransactionPrinterSpec.js
@@ -0,0 +1,26 @@
+describe("TransactionPrinter", function(){
+  var printer;
+  var transaction;
+
+  beforeEach(function(){
+    printer = new TransactionPrinter();
+    transaction = {
+      getDate: function(){},
+      getType: function(){},
+      getAmount: function(){},
+      getNewBalance: function(){},
+    };
+
+    spyOn(transaction, 'getDate').and.returnValue('01/01/2001');
+    spyOn(transaction, 'getType').and.returnValue('credit');
+    spyOn(transaction, 'getAmount').and.returnValue('100.00');
+    spyOn(transaction, 'getNewBalance').and.returnValue('200.00');
+  })
+
+  describe("#showHistory", function(){
+    it("Should print the transaction history", function(){
+      match = "date || credit || debit || balance\n01/01/2001 || 100.00 || 200.00"
+      expect(printer.printHistory([transaction])).toEqual(match);
+    })
+  })
+})
--- jasmine/src/TransactionPrinter.js
+++ jasmine/src/TransactionPrinter.js
@@ -1,7 +1,18 @@
 class TransactionPrinter{
-  constructor(){}
 
-  showHistory(){
-
+  printHistory(transactionList){
+    var header = "date || credit || debit || balance";
+    var returnStr = "";
+    for(var i = 0; i < transactionList.length; i++){
+      returnStr += "\n";
+      returnStr += transactionList[i].getDate();
+      if(transactionList[i].getType() == "credit"){
+        returnStr += " || " + transactionList[i].getAmount();
+      } else {
+        returnStr += " || || " + transactionList[i].getAmount();
+      }
+      returnStr += " || " + transactionList[i].getNewBalance();
+    }
+    return header+returnStr;
   }
 }
